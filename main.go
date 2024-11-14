5d7c99f959eb0eeed80562cbcc4b9d28ed23c04a
DownerCase <downercase8@gmail.com>
2024-11-14 11:39:49 +0000

    Update example main

--- main.go
+++ main.go
@@ -2,31 +2,71 @@ package main
 
 import (
 	"fmt"
-	"github.com/DownerCase/ecal-go/ecal"
-	"github.com/DownerCase/ecal-go/ecal/publisher"
 	"time"
+
+	"github.com/DownerCase/ecal-go/ecal"
+	"github.com/DownerCase/ecal-go/ecal/protobuf/publisher"
+	"github.com/DownerCase/ecal-go/protos"
 )
 
 func main() {
+	// eCAL version as string and semantic version components
 	fmt.Println(ecal.GetVersionString())
-	version := ecal.GetVersion()
-	fmt.Println(version)
-	fmt.Println("Init: ", ecal.Initialize(ecal.NewConfig(), "Go eCAL!", ecal.C_Publisher|ecal.C_Subscriber|ecal.C_Logging))
-	defer func() { fmt.Println("Finalize: ", ecal.Finalize()) }()
-	fmt.Println("Changed name: ", ecal.SetUnitName("Something new"))
-	fmt.Println("Am ok?", ecal.Ok())
-	pub, err := publisher.New()
+	fmt.Println(ecal.GetVersion())
+
+	// Initialize eCAL with default config, the unit name "Go eCAL",
+	// and the Publisher, Subscriber and Logging components enabled
+	initResult := ecal.Initialize(
+		ecal.NewConfig(),
+		"Go eCAL!",
+		ecal.C_Publisher|ecal.C_Subscriber|ecal.C_Logging,
+	)
+	fmt.Println("Init:", initResult)
+
+	defer ecal.Finalize() // Shutdown eCAL at the end of the program
+
+	// Change the unit name
+	fmt.Println("Changed name:", ecal.SetUnitName("Go demo"))
+
+	// Check if the eCAL system is Ok.
+	// Other eCAL programs can send a message to cause ecal.Ok() to return false
+	// Typically used as a condition to terminate daemon-style programs
+	fmt.Println("eCAL ok?", ecal.Ok())
+
+	// Create new protobuf publisher
+	pub, err := publisher.New(&protos.Person{})
 	if err != nil {
 		panic("Failed to make new publisher")
 	}
-	defer publisher.Destroy(&pub)
-	if pub.Create("example topic") != nil {
+	defer pub.Delete() // Don't forget to delete the publisher when done!
+
+	person := &protos.Person{Id: 0, Name: "John", Email: "john@doe.net",
+		Dog:   &protos.Dog{Name: "Pluto"},
+		House: &protos.House{Rooms: 5},
+	}
+
+	if pub.Create("person") != nil {
 		panic("Failed to Create publisher")
 	}
-	fmt.Println("Got publisher: ", pub)
+
 	for idx := range 100 {
-		fmt.Println("Sending message")
-		pub.Messages <- []byte{'A', 'B', byte(idx)}
+		// Check if program has been requested to stop
+		if !ecal.Ok() {
+			fmt.Println("eCAL.Ok() is false; shutting down")
+			return
+		}
+
+		fmt.Println("Sending message ", idx)
+
+		// Update message to send
+		person.Id = int32(idx)
+
+		// Serialize and send protobuf message
+		if err := pub.Send(person); err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		// Delay next iteration
 		time.Sleep(1 * time.Second)
 	}
 }
