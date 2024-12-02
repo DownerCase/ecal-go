b42b208547e8a25ccc8db7f7681d43ee5a37f5aa
DownerCase <downercase8@gmail.com>
2024-12-02 16:16:26 +0000

    Defer subpage handling

--- cmd/monitor/tea.go
+++ cmd/monitor/tea.go
@@ -28,13 +28,11 @@ const (
 	page_logs
 	page_system
 	page_about
-	page_topic_detailed
 )
 
 type model struct {
-	page           Page
-	model_topics   model_topics
-	model_detailed model_detailed
+	page         Page
+	model_topics model_topics
 }
 
 type TickMsg time.Time
@@ -50,38 +48,14 @@ func (m *model) updatePage(msg tea.Msg) tea.Cmd {
 	switch m.page {
 	case page_topics:
 		cmd = m.model_topics.Update(msg)
-	case page_topic_detailed:
-		cmd = m.model_detailed.Update(msg)
 	}
 	return cmd
-}
-
-// Navigate down - i.e.: Handle Enter keypress
-func (m *model) navDown() {
-	switch m.page {
-	case page_topics:
-		m.transitionTo(page_topic_detailed)
-	}
-}
-
-// Navigate down - i.e.: Handle Esc keypress
-func (m *model) navUp() {
-	switch m.page {
-	case page_topic_detailed:
-		m.transitionTo(page_topics)
-	}
 }
 
 func (m *model) transitionTo(newPage Page) {
 	switch newPage {
 	case page_topics:
 		m.model_topics.Refresh()
-	case page_topic_detailed:
-		topic, is_subscriber, err := m.model_topics.GetSelectedId()
-		if err != nil {
-			return // Don't' transition
-		}
-		m.model_detailed.ShowTopic(topic, is_subscriber)
 	}
 	m.page = newPage
 }
@@ -95,12 +69,8 @@ func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "esc":
-			m.navUp()
 		case "q", "ctrl+c":
 			return m, tea.Quit
-		case "enter":
-			m.navDown()
 		case "1":
 			m.transitionTo(page_topics)
 		case "2":
@@ -129,8 +99,6 @@ func (m *model) View() string {
 	switch m.page {
 	case page_topics:
 		s.WriteString(m.model_topics.View())
-	case page_topic_detailed:
-		s.WriteString(m.model_detailed.View())
 	default:
 		s.WriteString(placeholderTab(m.page))
 	}
@@ -144,7 +112,7 @@ func (m *model) View() string {
 		"7: About",
 	}
 	s.WriteString("\n")
-	page := m.page % (page_about + 1)
+	page := m.page
 	tabs[page] = highlight.Render(tabs[page])
 	for _, tab := range tabs {
 		s.WriteString(tab)
@@ -155,7 +123,7 @@ func (m *model) View() string {
 
 func doCli() {
 
-	m := model{page_topics, NewTopicsModel(), NewDetailedModel()}
+	m := model{page_topics, NewTopicsModel()}
 	p = tea.NewProgram(&m)
 
 	if _, err := p.Run(); err != nil {
