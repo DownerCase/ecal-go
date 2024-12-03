c91334160cb861b9d7a220f894ace1f4cd3f7d71
DownerCase <downercase8@gmail.com>
2024-12-03 10:03:51 +0000

    CTypes: Drop separate string size fields

--- ecal/types/types.cpp
+++ ecal/types/types.cpp
@@ -20,12 +20,11 @@ CDatatype toCDataType(const eCAL::SDataTypeInformation &datatype) {
 
 CTopicId toCTopicId(const eCAL::Registration::STopicId &id) {
   return {
-      {id.topic_id.entity_id.data(),
-       safe_len(id.topic_id.entity_id.size()),
-       id.topic_id.process_id,
-       id.topic_id.host_name.data(),
-       safe_len(id.topic_id.host_name.size())},
-      id.topic_name.data(),
-      safe_len(id.topic_name.size())
+      {
+          id.topic_id.entity_id.data(),
+          id.topic_id.host_name.data(),
+          id.topic_id.process_id,
+      },
+      id.topic_name.data()
   };
 }
