a5892807c0f09ae48908adf9f2b12d48c38cf157
DownerCase <downercase8@gmail.com>
2024-12-03 15:34:47 +0000

    Make toCTypes container generic

--- ecal/types/types.hpp
+++ ecal/types/types.hpp
@@ -13,8 +13,8 @@ CTopicId toCType(const eCAL::Registration::STopicId &id);
 CTopicMon toCType(const eCAL::Monitoring::STopicMon &topic);
 CProcessMon toCType(const eCAL::Monitoring::SProcessMon &proc);
 
-template <typename CType, typename EcalType>
-std::vector<CType> toCTypes(const std::vector<EcalType> &ecaltypes) {
+template <typename CType, typename EcalType, template<typename> typename Container >
+std::vector<CType> toCTypes(const Container<EcalType> &ecaltypes) {
   std::vector<CType> ctypes{};
   ctypes.reserve(ecaltypes.size());
   for (const auto &ecaltype : ecaltypes) {
