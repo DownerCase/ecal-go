c415c4d70c3868c8b9f7d7eeebc9b902589a9af3
DownerCase <downercase8@gmail.com>
2024-12-16 12:08:34 +0000

    Move services page to subpage map model

--- cmd/monitor/page_services_main.go
+++ cmd/monitor/page_services_main.go
@@ -9,21 +9,11 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type ServicesPage int
-
-const (
-	subpage_services_main ServicesPage = iota
-	subpage_services_detailed
-)
-
-type model_services struct {
+type model_services_main struct {
 	table_services table.Model
-	subpage        ServicesPage
-	model_detailed model_service_detailed
-	NavKeys        NavKeyMap
 }
 
-func NewServicesModel() *model_services {
+func NewServicesMainModel() *model_services_main {
 	cols := []table.Column{
 		{Title: "ID", Width: 0}, // Hidden unique ID
 		{Title: "T", Width: 1},  // Type (Client/Server)
@@ -32,79 +22,24 @@ func NewServicesModel() *model_services {
 		{Title: "Tick", Width: 4},
 	}
 
-	return (&model_services{
+	return &model_services_main{
 		table_services: NewTable(cols),
-		subpage:        subpage_services_main,
-		model_detailed: NewDetailedServiceModel(),
-		NavKeys:        make(NavKeyMap),
-	}).Init()
-}
-
-func (m *model_services) Refresh() {
-	switch m.subpage {
-	case subpage_services_detailed:
-		m.model_detailed.Refresh()
-	default:
-		m.updateTable(nil)
 	}
 }
 
-func (m *model_services) Init() *model_services {
-	m.NavKeys["esc"] = func() tea.Cmd { m.navUp(); return nil }
-	m.NavKeys["enter"] = func() tea.Cmd { m.navDown(); return nil }
-	return m
+func (m *model_services_main) Refresh() {
+	m.updateTable(nil)
 }
 
-func (m *model_services) Update(msg tea.Msg) tea.Cmd {
-	if cmd, navigated := m.NavKeys.HandleMsg(msg); navigated {
-		return cmd
-	}
-
-	var cmd tea.Cmd
-	switch m.subpage {
-	case subpage_services_main:
-		switch msg := msg.(type) {
-		case tea.KeyMsg:
-			m.updateTable(msg)
-		}
-	case subpage_services_detailed:
-		cmd = m.model_detailed.Update(msg)
-	}
-	return cmd
+func (m *model_services_main) Update(msg tea.Msg) tea.Cmd {
+	return m.updateTable(msg)
 }
 
-func (m *model_services) View() string {
-	switch m.subpage {
-	case subpage_services_main:
-		return baseStyle.Render(m.table_services.View()) + "\n" + m.table_services.HelpView()
-	case subpage_services_detailed:
-		return m.model_detailed.View()
-	}
-	return "Invalid page"
+func (m *model_services_main) View() string {
+	return baseStyle.Render(m.table_services.View()) + "\n" + m.table_services.HelpView()
 }
 
-func (m *model_services) navDown() {
-	switch m.subpage {
-	case subpage_services_main:
-		id, isServer, err := m.getSelectedId()
-		if err != nil {
-			return // Can't transition
-		}
-		m.model_detailed.Id = id
-		m.model_detailed.IsServer = isServer
-		m.subpage = subpage_services_detailed
-		m.model_detailed.Refresh()
-	}
-}
-
-func (m *model_services) navUp() {
-	switch m.subpage {
-	case subpage_services_detailed:
-		m.subpage = subpage_services_main
-	}
-}
-
-func (m *model_services) getSelectedId() (string, bool, error) {
+func (m *model_services_main) GetSelectedId() (string, bool, error) {
 	row := m.table_services.SelectedRow()
 	if row == nil {
 		return "", false, errors.New("No processes")
@@ -112,7 +47,7 @@ func (m *model_services) getSelectedId() (string, bool, error) {
 	return row[0], row[1] == "S", nil
 }
 
-func (m *model_services) updateTable(msg tea.Msg) {
+func (m *model_services_main) updateTable(msg tea.Msg) (cmd tea.Cmd) {
 	rows := []table.Row{}
 	mon := monitoring.GetMonitoring(monitoring.MonitorClient | monitoring.MonitorServer)
 	for _, client := range mon.Clients {
@@ -122,7 +57,8 @@ func (m *model_services) updateTable(msg tea.Msg) {
 		rows = append(rows, serverToRow(server))
 	}
 	m.table_services.SetRows(rows)
-	m.table_services, _ = m.table_services.Update(msg)
+	m.table_services, cmd = m.table_services.Update(msg)
+	return
 }
 
 func serviceToRow(service monitoring.ServiceBase) table.Row {
