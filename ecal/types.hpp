32e52bb75b53d6f78a17336584f8f0df2759dbd3
DownerCase <downercase8@gmail.com>
2025-01-20 11:22:12 +0000

    Remove toCTypes

--- ecal/types.hpp
+++ ecal/types.hpp
@@ -25,12 +25,4 @@ containerTo(const SrcContainerT<SrcT> &src, DstT (*proj)(const SrcT &)) {
   return dst;
 }
 
-template <
-    typename CType,
-    typename EcalType,
-    template <typename> typename Container>
-std::vector<CType> toCTypes(const Container<EcalType> &ecaltypes) {
-  return containerTo<std::vector>(ecaltypes, toCType);
-}
-
 #endif
