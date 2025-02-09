874618be2cf9ece536150f79f95de6f7b01452b3
DownerCase <downercase8@gmail.com>
2025-02-09 12:35:18 +0000

    Add shortened simple sample

---
+++ samples/sample.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/DownerCase/ecal-go/ecal"
+	"github.com/DownerCase/ecal-go/ecal/string/publisher"
+	"github.com/DownerCase/ecal-go/ecal/string/subscriber"
+)
+
+func main() {
+	// Initialize eCAL
+	ecal.Initialize(
+		ecal.NewConfig(ecal.WithConsoleLogging(true), ecal.WithConsoleLogAll()),
+		"Go eCAL!",
+		ecal.CDefault,
+	)
+	defer ecal.Finalize() // Shutdown eCAL at the end of the program
+
+	// Send messages
+	go func() {
+		publisher, err := publisher.New("string topic")
+		if err != nil {
+			panic("Failed to make string publisher")
+		}
+		defer publisher.Delete()
+
+		for idx := 0; true; idx++ {
+			_ = publisher.Send("This is message ", idx)
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Receive messages
+	subscriber, err := subscriber.New("string topic")
+	if err != nil {
+		panic("Failed to Create string subscriber")
+	}
+	defer subscriber.Delete()
+
+	for ecal.Ok() {
+		msg, err := subscriber.Receive(time.Second * 2)
+		if err == nil {
+			fmt.Println("Received:", msg) //nolint:forbidigo
+		} else {
+			fmt.Println(err) //nolint:forbidigo
+		}
+	}
+}
